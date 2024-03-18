62980857285254a83d3f792954d572c82e7a9295
Liviu Szilagyi <szilagyiliviu@outlook.com>
2024-03-18 20:04:02 +0100

    Final touches

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,125 @@
-/*=============== SHOW MENU ===============*/
+/**
+ * =============== SHOW MENU ===============
+ *
+ * @format
+ */
 
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*=============== MENU SHOW ===============*/
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*=============== MENU HIDDEN ===============*/
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
 
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // remove click from nav links
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SHADOW HEADER ===============*/
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
 
+window.addEventListener("scroll", shadowHeader);
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact__form"),
+  contactMessage = document.getElementById("contact-message");
 
+const sendEmail = (e) => {
+  e.preventDefault();
 
-/*=============== SHOW SCROLL UP ===============*/ 
+  // service ID - templateID - Form - Publickey
+  emailjs
+    .sendForm(
+      "service_x20bvv7",
+      "template_ug2dcwn",
+      "#contact__form",
+      "-2o2EFUnqWAV_-F_e"
+    )
+    .then(
+      () => {
+        // Show sent message
+        contactMessage.textContent = "Message sent successfully!";
 
+        // Remove message after 5 seconds
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 3500);
+
+        // Clear inputs
+        contactForm.reset();
+      },
+      () => {
+        // Show error
+        contactMessage.textContent = "Message not sent (Service error)";
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
 
-/*=============== DARK LIGHT THEME ===============*/ 
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "bx bx-sun-line";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
